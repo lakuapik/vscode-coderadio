431bc8fcdd6a25e79c208f2a476f092f2c23b064
Andrea Arrighi <arrighi000@gmail.com>
2022-10-13 03:12:54 +0200

    added volume buttion functionality

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,11 @@ import { get as fetch } from 'https';
 import * as vscode from "vscode";
 
 let statusBar: vscode.StatusBarItem;
+let volumeValue: vscode.StatusBarItem;
 let terminal: vscode.Terminal;
+let volume: number = 70
+let playingState = false;
+let platform: string = osPlatform()
 
 const radio = "https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3";
 const portal = "https://detectportal.firefox.com/success.txt";
@@ -19,7 +23,7 @@ async function hasConnection()
 function getPlayer()
 {
   let vlc = "/usr/bin/vlc"; // gnu/linux or mac?
-  if (osPlatform().includes('win')) {vlc = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe";}
+  if (platform.includes('win')) { vlc = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"; }
   return vscode.workspace.getConfiguration("coderadio").get("vlc_path") || vlc;
 }
 
@@ -34,13 +38,23 @@ function updateSidebar(text: string, tooltip: string, command: string)
 async function startTerminal()
 {
   terminal?.dispose(); // if there is a running instance
-  terminal = vscode.window.createTerminal({ 
-    shellPath: "C:\\Windows\\System32\\cmd.exe", // ensures that the default Windows terminal will open 
-    name: "coderadio",
-    hideFromUser: true
-  });
-  terminal.sendText(`"${getPlayer()}" ${radio} --intf dummy`);
+  if (platform.includes('win')) {
+    terminal = vscode.window.createTerminal({
+      shellPath: "C:\\Windows\\System32\\cmd.exe", // ensures that the default Windows terminal will open 
+      name: "coderadio",
+      hideFromUser: true
+    });
   setTimeout(() => terminal.dispose(), 3000); // it's not necessary to keep the terminal open in windows
+
+  } else {
+    terminal = vscode.window.createTerminal({
+      shellPath: "/bin/bash",
+      name: "coderadio",
+      hideFromUser: true
+    })
+  }
+  terminal.sendText(`"${getPlayer()}" ${radio} --intf dummy --gain ${volume / 100} &`);
+
 }
 
 function stopTerminal()
@@ -48,8 +62,8 @@ function stopTerminal()
   terminal?.dispose();
   // for some reason, dispose in windows doesnt kill its child processes
   // so we manually kill vlc with taskkill command
-  if (osPlatform().includes('win')) {
-    const t = vscode.window.createTerminal({ 
+  if (platform.includes('win')) {
+    const t = vscode.window.createTerminal({
       shellPath: "C:\\Windows\\System32\\cmd.exe", // ensures that the default Windows terminal will open
       hideFromUser: false
     });
@@ -64,35 +78,127 @@ async function playStream()
     vscode.window.showInformationMessage("Can't play, no internet connection.");
     return;
   }
-  updateSidebar("◼ Code Radio", "◼ Stop playing", "coderadio.stop");
+  updateSidebar("◼", "◼ Stop playing", "coderadio.stop");
+  playingState = true;
   startTerminal();
 }
 
-async function stopStream()
-{
-  updateSidebar("▶ Code Radio", "▶ Start playing", "coderadio.play");
+async function restartStream() {
+  stopTerminal();
+  if (!(await hasConnection())) {
+    vscode.window.showInformationMessage("Can't play, no internet connection.");
+    return;
+  }
+  startTerminal();
+}
+async function upVolume() {
+
+  if (volume <= 100) {
+    volume += 10;
+    refreshVolumeText()
+
+  }
+  if (playingState) {
+    restartStream()
+  }
+
+
+}
+async function downVolume() {
+
+  if (volume >= 0) {
+    volume -= 10;
+    refreshVolumeText()
+
+  }
+  if (playingState) {
+    restartStream()
+
+
+  }
+
+}
+
+
+async function stopStream() {
+  updateSidebar("▶", "▶ Start playing", "coderadio.play");
+  playingState = false;
+
+
   stopTerminal();
 }
 
-export function activate(context: vscode.ExtensionContext)
-{
-  let subscriptions = [
-    vscode.commands.registerCommand("coderadio.play", playStream),
-    vscode.commands.registerCommand("coderadio.stop", stopStream),
-  ];
+export function activate(context: vscode.ExtensionContext) {
+
 
   statusBar = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left,
     Number.MIN_SAFE_INTEGER
   );
-  subscriptions.push(statusBar);
 
+
+  let radioName = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    Number.MIN_SAFE_INTEGER
+  );
+
+  radioName.text = "Code Radio"
+
+  let { volumeUp, volumeDown } = initVolumeButtons()
+
+
+  let subscriptions = [
+    vscode.commands.registerCommand("coderadio.play", playStream),
+    vscode.commands.registerCommand("coderadio.stop", stopStream),
+    vscode.commands.registerCommand("coderadio.volumedown", downVolume),
+    vscode.commands.registerCommand("coderadio.volumeup", upVolume),
+    radioName,
+    statusBar,
+    volumeUp,
+    volumeDown
+
+
+  ];
   context.subscriptions.push(...subscriptions);
 
+  radioName.show()
   stopStream();
+  volumeUp.show()
+
+  volumeDown.show()
+  volumeValue.show()
 }
 
-export function deactivate()
-{
+function initVolumeButtons() {
+  let volumeUp = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    Number.MIN_SAFE_INTEGER
+  );
+  volumeUp.text = "+"
+
+  volumeUp.command = "coderadio.volumeup"
+
+  let volumeDown = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    Number.MIN_SAFE_INTEGER
+  );
+  volumeDown.text = "-"
+
+  volumeDown.command = "coderadio.volumedown"
+
+  volumeValue = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    Number.MIN_SAFE_INTEGER
+  );
+  refreshVolumeText()
+  return { volumeUp, volumeDown }
+}
+
+function refreshVolumeText() {
+  volumeValue.text = volume + "% 🕨"
+
+}
+
+export function deactivate() {
   stopStream();
 }
